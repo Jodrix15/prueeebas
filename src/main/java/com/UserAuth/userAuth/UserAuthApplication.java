628aaab6bc5f8803a08247aa24f50d4f4b792b13
Jodrix15 <146071125+Jodrix15@users.noreply.github.com>
2024-11-22 13:07:01 +0100

    token roles

--- src/main/java/com/UserAuth/userAuth/UserAuthApplication.java
+++ src/main/java/com/UserAuth/userAuth/UserAuthApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${prueeebas.dir}/.env")
 public class UserAuthApplication {
 
 	public static void main(String[] args) {
